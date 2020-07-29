b66239cb653d477fb7e55cb73db4f91243441cb3
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-29 19:47:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+using_include.include
 end
 
 def using_sort(array)
-
+using_sort.sort
 end
 
 def using_reverse(array)
-
+using_reverse.reverse
 end
 
 def using_first(array)
-
+using_first.first
 end
 
 def using_last(array)
-
+using_last.last
 end
 
 def using_size(array)
-
+using_size.size
 end
